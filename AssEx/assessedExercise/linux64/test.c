a5e924a0d5f0299570097ae7ccf0c0450bed4b28
2083008 <a.smithycchs@gmail.com>
2015-10-27 10:59:39 +0000

    string function now correct

--- AssEx/assessedExercise/linux64/test.c
+++ AssEx/assessedExercise/linux64/test.c
@@ -5,30 +5,27 @@
 char* hostname_strip(char *hostname);
 int main(void){
 
-	printf("%s\n", hostname_strip("glasgow.a.uk"));
+	printf("%s\n", hostname_strip("glasgowasdfasdfasdfasdf.a.us"));
 }
 
-char* hostname_strip(char *hostname)
+char* hostname_strip(char *hostname) //TODO
 {
-	char* stripped_string = (char*)malloc(4*sizeof(char));
-	stripped_string = 'uk';
-	printf("%s\n", hostname);
+	char* stripped_string = (char*)malloc(sizeof(char));
 	int temp = 0;
 	int i;
-	int length = strlen(stripped_string);
+	int length = strlen(hostname);
 	i = strlen(hostname);
 
 	for( i; hostname[i-1] != '.'; i-- ) // find the index of the last '.'
 	{
 		temp++;
 	}
-	
+	strncpy(stripped_string, hostname+(length-temp),temp);
     i = 0; // string to lower
     for( i; stripped_string[i]; i++)
     {
 		stripped_string[i] = tolower(stripped_string[i]);
 	}
-	stripped_string[temp+1] = '\0';
 	printf("%s\n",stripped_string);
 	return stripped_string;
 }
